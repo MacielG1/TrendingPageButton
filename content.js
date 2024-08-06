89c19bb548cba1725cef0f4e201401d8e682abef
MacielG1 <guflumaciel@gmail.com>
2024-08-06 10:10:22 -0300

    update button styles and optimize load timing

--- content.js
+++ content.js
@@ -1,63 +1,63 @@
-const targetDiv = document.querySelector('.dashboard-sidebar');
+const button = document.createElement('button');
+button.innerText = 'Trending';
 
-if (targetDiv) {
-  const button = document.createElement('button');
-  button.innerText = 'Trending';
-
-  function setButtonStyle(styles) {
-    for (const property in styles) {
-      button.style[property] = styles[property];
-    }
+function setButtonStyle(styles) {
+  for (const property in styles) {
+    button.style[property] = styles[property];
   }
+}
 
-  const defaultStyles = {
-    margin: '-10px 0 6px 25px',
-    padding: '4px 8px',
-    backgroundColor: 'transparent',
-    color: '#007BFF',
-    textDecoration: 'none',
-    borderRadius: '8px',
-    border: '1px solid #007BFF',
-    transition: 'background-color 0.3s, color 0.3s',
-    maxWidth: '5rem',
-  };
+const defaultStyles = {
+  padding: '4px 8px',
+  backgroundColor: 'transparent',
+  color: '#8d96a0',
+  textDecoration: 'none',
+  borderRadius: '8px',
+  border: '1px solid #30363d',
+  transition: 'background-color 0.3s, color 0.3s',
+  maxWidth: '5rem',
+};
 
-  const hoverStyles = {
-    backgroundColor: '#007BFF',
-    color: '#000',
-  };
+const hoverStyles = {
+  backgroundColor: '#b1bac41f',
+  color: '#8d96a0',
+};
 
-  const smallScreenStyles = {
-    margin: '2px 3px 15px 3px',
-    padding: '2px 4px',
-  };
+const smallScreenStyles = {
+  padding: '2px 4px',
+};
 
+setButtonStyle(defaultStyles);
+
+button.addEventListener('click', () => {
+  window.open('https://github.com/trending', '_self');
+});
+
+button.addEventListener('mouseover', () => setButtonStyle(hoverStyles));
+
+button.addEventListener('mouseout', () => {
   setButtonStyle(defaultStyles);
+  if (window.innerWidth < 400) setButtonStyle(smallScreenStyles);
+});
 
-  button.addEventListener('click', () => {
-    window.open('https://github.com/trending', '_self');
-  });
+window.addEventListener('resize', handleResize);
+handleResize();
 
-  button.addEventListener('mouseover', () => setButtonStyle(hoverStyles));
+function handleResize() {
+  const appHeaderSearch = document.querySelector('.AppHeader-search');
+  const dBlockElement = document.querySelector('.d-block');
 
-  button.addEventListener('mouseout', () => {
+  if (window.innerWidth < 300) {
+    setButtonStyle(smallScreenStyles);
+    if (dBlockElement) {
+      dBlockElement.parentNode.insertBefore(button, dBlockElement.previousSibling);
+    }
+  } else {
     setButtonStyle(defaultStyles);
-    if (window.innerWidth < 768) setButtonStyle(smallScreenStyles);
-  });
-
-  targetDiv.appendChild(button);
-
-  window.addEventListener('resize', handleResize);
-  handleResize();
-
-  function handleResize() {
-    const element = document.querySelector('.d-block');
-    if (window.innerWidth < 768) {
-      setButtonStyle(smallScreenStyles);
-      if (element) element.parentNode.insertBefore(button, element.nextSibling);
-    } else {
-      setButtonStyle(defaultStyles);
-      targetDiv.appendChild(button);
+    if (appHeaderSearch) {
+      appHeaderSearch.parentNode.insertBefore(button, appHeaderSearch);
     }
   }
 }
+
+handleResize();
